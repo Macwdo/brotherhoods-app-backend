1e575d10f25af7c08433b26ed08b322169995d5b
macwdo <danilo.macedofernandes@hotmail.com>
2024-03-24 00:37:07 -0300

    fixing to localtime

--- app_platform/utils.py
+++ app_platform/utils.py
@@ -1,12 +1,12 @@
 from datetime import timedelta
 from django.utils import timezone
 def get_next_wednesday_date():
-    today = timezone.localdate()
+    today = timezone.localtime()
     days_until_wednesday = (2 - today.weekday()) % 7
     return today + timedelta(days=days_until_wednesday)
 
 
 def get_previous_wednesday_date():
-    today = timezone.localdate()
+    today = timezone.localtime()
     days_to_substract = (today.weekday() - 2) % 7
     return today + timedelta(days=days_to_substract)
